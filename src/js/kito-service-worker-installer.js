c491a441f856d41e2288f7c055fe12cfe792dccf
TheKito <11139894+TheKito@users.noreply.github.com>
2022-06-16 05:24:57 -0300

    SW installer

---
+++ src/js/kito-service-worker-installer.js
@@ -0,0 +1,23 @@
+"use strict";
+
+(function ()
+{
+    if (('serviceWorker' in navigator))
+    {
+        navigator.serviceWorker.register('/serviceWorker.js')
+                .then(function (registration) {
+                    registration.addEventListener('updatefound', function ()
+                    {
+                        var installingWorker = registration.installing;
+                        console.log('A new service worker is being installed:', installingWorker);
+                        window.setTimeout(function ()
+                        {
+                            location.reload();
+                        }, 1000);
+                    });
+                })
+                .catch(function (error) {
+                    console.error('Service worker registration failed:', error);
+                });
+    }
+}());
