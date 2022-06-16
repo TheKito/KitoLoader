0f7797b9eea42730135acfe7b574dc6e4b4e7371
TheKito <11139894+TheKito@users.noreply.github.com>
2022-06-16 05:24:28 -0300

    getUserAgent

--- src/js/kito-browser.js
+++ src/js/kito-browser.js
@@ -14,7 +14,7 @@ window.kito.browser = (function () {
             return (('standalone' in window.navigator) && (window.navigator.standalone)) || ((window.matchMedia('(display-mode: standalone)').matches));
         },
         isSafari: function () {
-            return ('vendor' in navigator) && navigator.vendor.indexOf('Apple') > -1 && window.getUserAgent().indexOf('crios') == -1 && window.getUserAgent().indexOf('fxios') == -1;
+            return ('vendor' in navigator) && navigator.vendor.indexOf('Apple') > -1 && getUserAgent().indexOf('crios') == -1 && getUserAgent().indexOf('fxios') == -1;
         },
         isChrome: function () {
             return /chrome/.test(getUserAgent()) && /Google Inc/.test(navigator.vendor);
